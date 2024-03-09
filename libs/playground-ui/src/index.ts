314a6e617e32166fa62764ffb6711930837c2bfb
axelvanhelvoirt <axelvhelvoirt@gmail.com>
2024-03-09 21:14:23 +0100

    removed playground-ui.ts

--- libs/playground-ui/src/index.ts
+++ libs/playground-ui/src/index.ts
@@ -1,3 +1,2 @@
-export * from './lib/playground-ui';
 export * from './lib/my-component';
 export * from './lib/playground-button';
--- libs/playground-ui/src/lib/playground-ui.ts
+++
@@ -1,3 +0,0 @@
-export function playgroundUi(): string {
-  return 'playground-ui';
-}
